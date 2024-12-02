44048449d5a208e0ff388ff61fe897fbc0048f9a
Yi Zhang <58002595+yizhang24@users.noreply.github.com>
2024-12-02 18:35:59 -0500

    Read calib data

--- firmware/bmp338/bmp338.cpp
+++ firmware/bmp338/bmp338.cpp
@@ -1,15 +1,16 @@
 #include "bmp338.h"
 
-const i2c_inst_t* I2C_PORT = i2c0;
-const uint SDA_PIN = 4;
-const uint SCL_PIN = 5;
-const uint I2C_BAUDRATE = 100000;
+i2c_inst_t* I2C_PORT = i2c0;
+uint SDA_PIN = 4;
+uint SCL_PIN = 5;
+uint I2C_BAUDRATE = 100000;
 
 // Initialize
 // Compensation values
 // Pressure data
 
-static uint8_t i2c_read(uint8_t device_addr, uint8_t reg, uint8_t* data, uint16_t len) {
+// will add spi function later
+static uint8_t i2c_spi_read(uint8_t device_addr, uint8_t reg, uint8_t* data, uint16_t len) {
 	if (i2c_write_blocking(I2C_PORT, device_addr, &reg, 1, true) < 0) {
 		return 1;
 	}
@@ -20,21 +21,110 @@ static uint8_t i2c_read(uint8_t device_addr, uint8_t reg, uint8_t* data, uint16_
 	return 0;
 }
 
-static uint8_t i2c_write(uint8_t device_addr, uint8_t reg, uint8_t *buf, uint8_t len) {
-	for (i = 0; i < len; i++) {
+// will add spi function later
+static uint8_t i2c_spi_write(uint8_t device_addr, uint8_t reg, uint8_t *buf, uint8_t len) {
+	for (int i = 0; i < len; i++) {
 		uint8_t buffer[2];
-		buffer[0] = reg + i;
-		buffer[1] = buf + i;
-		if (i2c_write_blocking(I2C_PORT, device_addr, buffer, 2) < 0) {
+		buffer[0] = reg+i;
+		memcpy(&buffer[1], buf, 1);
+		if (i2c_write_blocking(I2C_PORT, device_addr, buffer, 2, false) < 0) {
 			return 1;
 		}
 	}
 	return 0;
 }
 
+static uint8_t get_calibration_data(bmp388_handle_s *handle) {
+	uint8_t buf[2];
+	if (i2c_spi_read(handle->iic_addr, BMP388_REG_NVM_PAR_T1_L, buf, 2) == 1) {
+		printf("BMP338: Failed to read PAR_T1");
+		return 1;
+	}
+	handle->t1 = (uint16_t)buf[1] << 8 | buf[0];
+
+	if (i2c_spi_read(handle->iic_addr, BMP388_REG_NVM_PAR_T2_L, buf, 2) == 1) {
+		printf("BMP338: Failed to read PAR_T2");
+		return 1;
+	}
+	handle->t2 = (uint16_t)buf[1] << 8 | buf[0];
+
+	if (i2c_spi_read(handle->iic_addr, BMP388_REG_NVM_PAR_T3, buf, 1) == 1) {
+		printf("BMP338: Failed to read PAR_T3");
+		return 1;
+	}
+	handle->t3 = (uint8_t)buf[0];
+	
+	if (i2c_spi_read(handle->iic_addr, BMP388_REG_NVM_PAR_P1_L, buf, 2) == 1) {
+		printf("BMP338: Failed to read PAR_P1");
+		return 1;
+	}
+	handle->p1 = (uint16_t)buf[1] << 8 | buf[0];
+
+	if (i2c_spi_read(handle->iic_addr, BMP388_REG_NVM_PAR_P2_L, buf, 2) == 1) {
+		printf("BMP338: Failed to read PAR_P2");
+		return 1;
+	}
+	handle->p2 = (uint16_t)buf[1] << 8 | buf[0];
+
+	if (i2c_spi_read(handle->iic_addr, BMP388_REG_NVM_PAR_P3, buf, 1) == 1) {
+		printf("BMP338: Failed to read PAR_P3");
+		return 1;
+	}
+	handle->p3 = (uint8_t)buf[0];
+	
+	if (i2c_spi_read(handle->iic_addr, BMP388_REG_NVM_PAR_P4, buf, 1) == 1) {
+		printf("BMP338: Failed to read PAR_P4");
+		return 1;
+	}
+	handle->p4 = (uint8_t)buf[0];
+
+	if (i2c_spi_read(handle->iic_addr, BMP388_REG_NVM_PAR_P5_L, buf, 2) == 1) {
+		printf("BMP338: Failed to read PAR_P5");
+		return 1;
+	}
+	handle->p5 = (uint16_t)buf[1] << 8 | buf[0];
+
+	if (i2c_spi_read(handle->iic_addr, BMP388_REG_NVM_PAR_P6_L, buf, 2) == 1) {
+		printf("BMP338: Failed to read PAR_P6");
+		return 1;
+	}
+	handle->p6 = (uint16_t)buf[1] << 8 | buf[0];
+
+	if (i2c_spi_read(handle->iic_addr, BMP388_REG_NVM_PAR_P7, buf, 1) == 1) {
+		printf("BMP338: Failed to read PAR_P7");
+		return 1;
+	}
+	handle->p7 = (uint8_t)buf[0];
+	
+	if (i2c_spi_read(handle->iic_addr, BMP388_REG_NVM_PAR_P8, buf, 1) == 1) {
+		printf("BMP338: Failed to read PAR_P8");
+		return 1;
+	}
+	handle->p8 = (uint8_t)buf[0];
+
+	if (i2c_spi_read(handle->iic_addr, BMP388_REG_NVM_PAR_P9_L, buf, 2) == 1) {
+		printf("BMP338: Failed to read PAR_P9");
+		return 1;
+	}
+	handle->p9 = (uint16_t)buf[1] << 8 | buf[0];
+
+	if (i2c_spi_read(handle->iic_addr, BMP388_REG_NVM_PAR_P10, buf, 1) == 1) {
+		printf("BMP338: Failed to read PAR_P10");
+		return 1;
+	}
+	handle->p10 = (uint8_t)buf[0];
+	
+	if (i2c_spi_read(handle->iic_addr, BMP388_REG_NVM_PAR_P11, buf, 1) == 1) {
+		printf("BMP338: Failed to read PAR_P11");
+		return 1;
+	}
+	handle->p11 = (uint8_t)buf[0];
+	
+	return 0;
+}
+
 int main(int argc, char const *argv[])
 {
 	stdio_init_all();
-	i2c_init()
 	return 0;
 }
--- firmware/bmp338/bmp338.h
+++ firmware/bmp338/bmp338.h
@@ -1,5 +1,8 @@
-#include "pico/stdlib.h"
+#include <cstring>
 #include "hardware/i2c.h"
+#include "pico/stdlib.h"
+#include <stdio.h>
+#include "pico/stdio.h"
 
 typedef enum {
 	BMP338_REG_CMD           = 0x7E,
@@ -10,25 +13,71 @@ typedef enum {
 	BMP338_REG_DATA_1        = 0x05,
 	BMP338_REG_DATA_2        = 0x06,
 	BMP338_REG_DATA_3        = 0x07,
-	BMP338_REG_DATA_4        = 0X08,
-	BMP338_REG_DATA_5        = 0X09,
-	BMP338_REG_SENSORTIME_0  = 0X0C,
-	BMP338_REG_SENSORTIME_1  = 0X0D,
-	BMP338_REG_SENSORTIME_2  = 0X0E,
-	BMP338_REG_EVENT         = 0X10,
-	BMP338_REG_INT_STATUS    = 0X11,
-	BMP338_REG_FIFO_LENGTH_0 = 0X12,
-	BMP338_REG_FIFO_LENGTH_1 = 0X13,
-	BMP338_REG_FIFO_DATA     = 0X14,
-	BMP338_REG_FIFO_WTM_0    = 0X15,
-	BMP338_REG_FIFO_WTM_1    = 0X16,
-	BMP338_REG_FIFO_CONFIG_1 = 0X17,
-	BMP338_REG_FIFO_CONFIG_2 = 0X18,
-	BMP338_REG_INT_CTRL      = 0X19,
-	BMP338_REG_IF_CONF       = 0X1A,
-	BMP338_REG_PWR_CTRL      = 0X1B,
-	BMP338_REG_OSR           = 0X1C,
-	BMP338_REG_ODR           = 0X1D,
-	BMP338_REG_CONFIG        = 0X1F
+	BMP338_REG_DATA_4        = 0x08,
+	BMP338_REG_DATA_5        = 0x09,
+	BMP338_REG_SENSORTIME_0  = 0x0C,
+	BMP338_REG_SENSORTIME_1  = 0x0D,
+	BMP338_REG_SENSORTIME_2  = 0x0E,
+	BMP338_REG_EVENT         = 0x10,
+	BMP338_REG_INT_STATUS    = 0x11,
+	BMP338_REG_FIFO_LENGTH_0 = 0x12,
+	BMP338_REG_FIFO_LENGTH_1 = 0x13,
+	BMP338_REG_FIFO_DATA     = 0x14,
+	BMP338_REG_FIFO_WTM_0    = 0x15,
+	BMP338_REG_FIFO_WTM_1    = 0x16,
+	BMP338_REG_FIFO_CONFIG_1 = 0x17,
+	BMP338_REG_FIFO_CONFIG_2 = 0x18,
+	BMP338_REG_INT_CTRL      = 0x19,
+	BMP338_REG_IF_CONF       = 0x1A,
+	BMP338_REG_PWR_CTRL      = 0x1B,
+	BMP338_REG_OSR           = 0x1C,
+	BMP338_REG_ODR           = 0x1D,
+	BMP338_REG_CONFIG        = 0x1F,
+	BMP388_REG_NVM_PAR_T1_L  = 0x31,
+	BMP388_REG_NVM_PAR_T1_H  = 0x32,
+	BMP388_REG_NVM_PAR_T2_L  = 0x33,
+	BMP388_REG_NVM_PAR_T2_H  = 0x34,
+	BMP388_REG_NVM_PAR_T3    = 0x35,
+	BMP388_REG_NVM_PAR_P1_L  = 0x36,
+	BMP388_REG_NVM_PAR_P1_H  = 0x37,
+	BMP388_REG_NVM_PAR_P2_L  = 0x38,
+	BMP388_REG_NVM_PAR_P2_H  = 0x39,
+	BMP388_REG_NVM_PAR_P3    = 0x3A,
+	BMP388_REG_NVM_PAR_P4    = 0x3B,
+	BMP388_REG_NVM_PAR_P5_L  = 0x3C,
+	BMP388_REG_NVM_PAR_P5_H  = 0x3D,
+	BMP388_REG_NVM_PAR_P6_L  = 0x3E,
+	BMP388_REG_NVM_PAR_P6_H  = 0x3F,
+	BMP388_REG_NVM_PAR_P7    = 0x40,
+	BMP388_REG_NVM_PAR_P8    = 0x41,
+	BMP388_REG_NVM_PAR_P9_L  = 0x42,
+	BMP388_REG_NVM_PAR_P9_H  = 0x43,
+	BMP388_REG_NVM_PAR_P10   = 0x44,
+	BMP388_REG_NVM_PAR_P11   = 0x45
 
 } bmp338_register_t;
+
+/**
+ * @brief bmp388 handle structure definition
+ */
+typedef struct bmp388_handle_s
+{
+    uint8_t iic_addr;                                                                   /**< iic device address */
+    uint8_t inited;                                                                     /**< inited flag */
+    uint8_t iic_spi;                                                                    /**< iic spi interface */
+    uint16_t t1;                                                                        /**< t1 register */
+    uint16_t t2;                                                                        /**< t2 register */
+    int8_t t3;                                                                          /**< t3 register */
+    int16_t p1;                                                                         /**< p1 register */
+    int16_t p2;                                                                         /**< p2 register */
+    int8_t p3;                                                                          /**< p3 register */
+    int8_t p4;                                                                          /**< p4 register */
+    uint16_t p5;                                                                        /**< p5 register */
+    uint16_t p6;                                                                        /**< p6 register */
+    int8_t p7;                                                                          /**< p7 register */
+    int8_t p8;                                                                          /**< p8 register */
+    int16_t p9;                                                                         /**< p9 register */
+    int8_t p10;                                                                         /**< p10 register */
+    int8_t p11;                                                                         /**< p11 register */
+    int64_t t_lin;                                                                      /**< t_lin register */
+} bmp388_handle_t;
